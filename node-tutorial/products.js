2a4d0fffb3d12dcd777943503b9ede7163ec6a98
Lusindiso <lusindiso@projectcodex.co>
2016-03-23 16:08:05 +0200

    lusindiso

--- node-tutorial/products.js
+++ node-tutorial/products.js
@@ -1,9 +1,34 @@
 var fs = require("fs");
 
-var products = [];
+  module.exports = function(filepath){//always make sure that you export the module so that its gonna be visible
 
+    this.productNames = function(callback){ //ccallback tells me that I am reading the file Asynch
+
+var products = [];
+var map = {};
+
+    //reading the csv file
     var listInFiles = fs.readFileSync("./files/products.csv", "utf8");
     var lines = listInFiles.split('\n');
-    products.push(lines);
+    // products.push(lines);
 
-    console.log(products);
+    //using the forEach to go through the lines youve splitted..
+    lines.forEach(function(lines) {
+       var productName = lines.split(",")[1];//splitting the lines
+       if(map[productName]=== undefined){//if the product is already there then do not push it to the array again
+          products.push(productName); //pushing the names to the array
+          map[productName]= 0; // always initialise your map
+       }
+
+    });
+
+
+            callback(null, products);
+
+
+
+}
+      this.productsSold = function(callback){
+
+}
+};
